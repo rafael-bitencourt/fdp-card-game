fa2c542f35f1232e79ad05de47f633f4140a304a
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-04-09 10:12:54 -0300

    fix: corrigido o caminho para o assets

--- interface/main.py
+++ interface/main.py
@@ -41,18 +41,18 @@ janela = Tk()
 janela.geometry("1012x759")
 janela.resizable(False, False)
 janela.title("Interface Filho da Puta")
-janela.iconbitmap("interface/assets/icone.ico")
+#janela.iconbitmap("interface/assets/icone.ico")
 
 #Configurando a imagem de fundo
-mesa = PhotoImage(file="interface/assets/mesa-interface.png")
+mesa = PhotoImage(file="assets/mesa-interface.png")
 label_mesa = Label(janela, image=mesa)
 label_mesa.place(x=0, y=0, relwidth=1, relheight=1)
 
 #Configurando quantas_faz
-imagem_botao_0 = ImageTk.PhotoImage(Image.open("interface/assets/botao-0.png"))
-imagem_botao_1 = ImageTk.PhotoImage(Image.open("interface/assets/botao-1.png"))
-imagem_botao_2 = ImageTk.PhotoImage(Image.open("interface/assets/botao-2.png"))
-imagem_botao_3 = ImageTk.PhotoImage(Image.open("interface/assets/botao-3.png"))
+imagem_botao_0 = ImageTk.PhotoImage(Image.open("assets/botao-0.png"))
+imagem_botao_1 = ImageTk.PhotoImage(Image.open("assets/botao-1.png"))
+imagem_botao_2 = ImageTk.PhotoImage(Image.open("assets/botao-2.png"))
+imagem_botao_3 = ImageTk.PhotoImage(Image.open("assets/botao-3.png"))
 
 botao_0 = Button(janela, command=lambda: quantas_faz(0), height=40, width=40, image=imagem_botao_0, bd=0, bg="#A5D744")
 botao_1 = Button(janela, command=lambda: quantas_faz(1), height=40, width=40, image=imagem_botao_1, bd=0, bg="#A5D744")
@@ -65,27 +65,27 @@ botao_2.place(x=519, y=524)
 botao_3.place(x=569, y=524)
 
 #Configurando jogar_carta
-imagem_carta_1 = ImageTk.PhotoImage(Image.open("interface/assets/carta-1.png"))
-imagem_carta_2 = ImageTk.PhotoImage(Image.open("interface/assets/carta-2.png"))
-imagem_carta_3 = ImageTk.PhotoImage(Image.open("interface/assets/carta-3.png"))
+imagem_carta_1 = ImageTk.PhotoImage(Image.open("assets/carta-1.png"))
+imagem_carta_2 = ImageTk.PhotoImage(Image.open("assets/carta-2.png"))
+imagem_carta_3 = ImageTk.PhotoImage(Image.open("assets/carta-3.png"))
 
 carta_1 = Button(janela, command=lambda: jogar_carta(1), width=73, height=102, image=imagem_carta_1, bd=3)
 carta_2 = Button(janela, command=lambda: jogar_carta(2), width=73, height=102, image=imagem_carta_2, bd=3)
 carta_3 = Button(janela, command=lambda: jogar_carta(3), width=73, height=102, image=imagem_carta_3, bd=3)
 
 #Configurando outros assets
-quadrado_verde = ImageTk.PhotoImage(Image.open("interface/assets/quadrado-verde.png"))
-off_1 = ImageTk.PhotoImage(Image.open("interface/assets/1-off.png"))
-on_2 = ImageTk.PhotoImage(Image.open("interface/assets/2-on.png"))
+quadrado_verde = ImageTk.PhotoImage(Image.open("assets/quadrado-verde.png"))
+off_1 = ImageTk.PhotoImage(Image.open("assets/1-off.png"))
+on_2 = ImageTk.PhotoImage(Image.open("assets/2-on.png"))
 
 label_quadrado_verde = Label(janela, image=quadrado_verde, bd=0)
 label_off_1 = Label(janela, image=off_1, bd=0)
 label_on_2 = Label(janela, image=on_2, bd=0)
 
 #Configurando disse
-disse_0 = ImageTk.PhotoImage(Image.open("interface/assets/disse-0.png"))
-disse_2 = ImageTk.PhotoImage(Image.open("interface/assets/disse-2.png"))
-disse_3 = ImageTk.PhotoImage(Image.open("interface/assets/disse-3.png"))
+disse_0 = ImageTk.PhotoImage(Image.open("assets/disse-0.png"))
+disse_2 = ImageTk.PhotoImage(Image.open("assets/disse-2.png"))
+disse_3 = ImageTk.PhotoImage(Image.open("assets/disse-3.png"))
 
 label_disse_0 = Label(janela, image=disse_0, bd=0)
 label_disse_2 = Label(janela, image=disse_2, bd=0)
