5b1577cf9c743973b2b7071c840f5008d0009cf3
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-04-30 14:12:18 -0300

    adicionado método que verifica vencedor da mesa

--- src/mesa.py
+++ src/mesa.py
@@ -14,3 +14,18 @@ class Mesa:
     
     def set_jogadores_na_mesa(self, jogadores):
         self.__jogadores_na_mesa = jogadores
+    
+    def verificar_vencedor_mesa(self):
+        carta_mais_alta = None
+        vencedor = None
+        
+        # Coleta as cartas jogadas na mesa por cada jogador
+        for jogador in self.__jogadores_na_mesa:
+            carta_jogada = jogador.get_carta_jogada()
+            
+            # Verifica se a carta atual é mais alta do que a carta mais alta já registrada
+            if carta_mais_alta is None or carta_jogada.valor > carta_mais_alta.valor:
+                carta_mais_alta = carta_jogada
+                vencedor = jogador
+                
+        return vencedor
