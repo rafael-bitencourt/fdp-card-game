5a30a94d8f93991d1a09020343d1dcc6b2707889
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-06-07 21:50:01 -0300

    fix: get_valor e get_naipe corrigidos

--- src/carta.py
+++ src/carta.py
@@ -1,37 +1,13 @@
 class Carta:
     forca_cartas = {
-        "A espadas": 14,
-        "A paus": 13,
-        "7 espadas": 12,
-        "7 ouros": 11,
-        "A copas": 10,
-        "A ouros": 10,
-        "K espadas": 9,
-        "K copas": 9,
-        "K ouros": 9,
-        "K paus": 9,
-        "Q espadas": 8,
-        "Q copas": 8,
-        "Q ouros": 8,
-        "Q paus": 8,
-        "J espadas": 7,
-        "J copas": 7,
-        "J ouros": 7,
-        "J paus": 7,
-        "7 copas": 6,
-        "7 paus": 6,
-        "6 espadas": 5,
-        "6 copas": 5,
-        "6 ouros": 5,
-        "6 paus": 5,
-        "5 espadas": 4,
-        "5 copas": 4,
-        "5 ouros": 4,
-        "5 paus": 4,
-        "4 espadas": 3,
-        "4 copas": 3,
-        "4 ouros": 3,
-        "4 paus": 3,
+        "A de espadas": 14, "A de paus": 13, "7 de espadas": 12, "7 de ouros": 11,
+        "A de copas": 10, "A de ouros": 10, "K de espadas": 9, "K de copas": 9,
+        "K de ouros": 9, "K de paus": 9, "Q de espadas": 8, "Q de copas": 8,
+        "Q de ouros": 8, "Q de paus": 8, "J de espadas": 7, "J de copas": 7,
+        "J de ouros": 7, "J de paus": 7, "7 de copas": 6, "7 de paus": 6,
+        "6 de espadas": 5, "6 de copas": 5, "6 de ouros": 5, "6 de paus": 5,
+        "5 de espadas": 4, "5 de copas": 4, "5 de ouros": 4, "5 de paus": 4,
+        "4 de espadas": 3, "4 de copas": 3, "4 de ouros": 3, "4 de paus": 3,
     }
 
     def __init__(self, valor, naipe):
@@ -42,10 +18,10 @@ class Carta:
         return f"{self.valor} de {self.naipe}"
     
     def get_valor(self):
-        return self.valor
+        return self.__valor
 
     def get_naipe(self):
-        return self.naipe
+        return self.__naipe
 
     def get_forca(self):
         return Carta.forca_cartas[f"{self.get_valor()} {self.get_naipe()}"]
