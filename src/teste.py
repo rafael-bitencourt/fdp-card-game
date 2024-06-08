a28029327a6fcdb637f30fbdff686b99f17907cf
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-06-08 16:18:54 -0300

    teste de linkar carta clicada na in terface com objeto carta

---
+++ src/teste.py
@@ -0,0 +1,22 @@
+from carta import Carta
+import tkinter as tk
+from tkinter import PhotoImage
+
+root = tk.Tk()
+
+
+imagens_cartas = {}  # Dicionário para armazenar as PhotoImages
+lista_cartas = [Carta("2", "copas"), Carta("6", "copas"), Carta("3", "espadas")]  # Lista de cartas
+
+for carta in lista_cartas:
+    path = f"assets/{carta.get_valor()} {carta.get_naipe()}.png"
+    imagens_cartas[carta] = PhotoImage(file=path)
+
+def acao_carta(carta):
+    print(f"Carta clicada: {carta.get_valor()} de {carta.get_naipe()}")
+
+for carta, img in imagens_cartas.items():
+    botao = tk.Button(root, image=img, command=lambda c=carta: acao_carta(c))
+    botao.pack()  
+
+root.mainloop()
