0bf7ba9b7daeda6649ef2f320d3fbab83f8056af
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-06-08 21:40:30 -0300

    add método computar vencedor do jogo

--- src/jogo.py
+++ src/jogo.py
@@ -1,4 +1,23 @@
+import math
+
 class Jogo:
     def __init__(self, jogadores, indice_local):
         self.__jogadores = jogadores
         self.__indice_local = indice_local
+    
+    def get_jogadores(self):
+        return self.__jogadores
+    
+    def get_indice_local(self):
+        return self.__indice_local
+
+    def computar_vencedor_jogo(self):
+        jogadores = self.get_jogadores()
+        menor = math.inf
+        for i in range(len(jogadores)):
+            if jogadores[i].get_total_pontos() < menor:
+                menor = jogadores[i].get_total_pontos()
+                jogador_menos_pontos = jogadores[i]   
+        return jogador_menos_pontos
+        
+        
