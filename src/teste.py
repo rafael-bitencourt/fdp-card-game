8b84a7d6a362d71ade4cacfe4e7cf23fc10fcba0
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-06-17 22:00:39 -0300

    add teste

--- src/teste.py
+++ src/teste.py
@@ -6,8 +6,8 @@ from baralho import Baralho
 baralho = Baralho()
 baralho.embaralhar()
 
-jogador1 = Jogador("Jogador 1", 1)
-jogador1.set_cartas_jogador(baralho.retirar_cartas(3))
+jogador1 = Jogador(["Jogador 1", 1, 100])
+jogador1.set_cartas_jogador(baralho.retirar_cartas(5))
 
 root = tk.Tk()
 
@@ -24,11 +24,12 @@ def jogar_carta(carta):
     # Desabilitar o botão da carta jogada
     botao = botoes_cartas[carta]
     botao.destroy()  # Desabilita o botão
-    print(f"Carta jogada: {carta.get_valor()} de {carta.get_naipe()}")
+    print(f"Carta jogada: {jogador1.get_carta_jogada().get_valor()} de {jogador1.get_carta_jogada().get_naipe()}")
+    print(f"Cartas restantes: {[carta.get_valor() for carta in jogador1.get_cartas_jogador()]}")
 
 
 for carta, img in imagens_cartas.items():
-    botao = tk.Button(root, image=img, command=lambda c=carta: jogar_carta(c))
+    botao = tk.Button(root, image=img, command=lambda carta=carta: jogar_carta(carta))
     botao.pack()
     botoes_cartas[carta] = botao  # Armazena o botão na tabela de correspondência
 
