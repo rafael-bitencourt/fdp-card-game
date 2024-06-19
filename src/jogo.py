680c50b42625a12324701e5a968bd14eaf469ebf
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2024-06-18 22:07:56 -0300

    add lógica de jogo criar rodada

--- src/jogo.py
+++ src/jogo.py
@@ -1,13 +1,27 @@
 import math
+from rodada import Rodada
 
 class Jogo:
     def __init__(self, jogadores, jogador_local):
         self.__jogadores = jogadores
         self.__jogador_local = jogador_local
+        self.__numero_da_rodada = 1
+        self.__rodada_atual = Rodada(self.__numero_da_rodada)
+        
     
     def get_jogadores(self):
         return self.__jogadores
     
+    def get_rodada_atual(self):
+        return self.__rodada_atual
+    
+    def get_numero_da_rodada(self):
+        return self.__numero_da_rodada
+    
+    def criar_rodada(self):
+        self.____numero_da_rodada += 1
+        self.__rodada_atual = Rodada(self.____numero_da_rodada)
+        
     def get_jogador_local(self):
         return self.__jogador_local
 
